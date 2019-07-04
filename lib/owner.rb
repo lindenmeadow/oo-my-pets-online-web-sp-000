629d47c88325cb664fffdc3fc1f31b79e67a8de4
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-07-04 16:10:33 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -53,7 +53,7 @@ class Owner
   end
 
   def list_pets
-    "I have #{pets[:dogs].length} dogs, and #{pets[:cats].length} cats."
+    "I have #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
   end
 
   def sell_pets
