1cc01fdccc8077dc5c76dd7c39333b3e92bccd66
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-07-04 16:28:44 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,8 +3,8 @@ class Cat
   attr_reader :name
   attr_accessor :mood
 
-  def initialize(name, mood = "nervous")
+  def initialize(name)
     @name = name
-    @mood = mood
+    @mood = "nervous"
   end
 end
